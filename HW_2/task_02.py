e91bae818c13f9f8c2b70a6f7393c71ac61a12bb
Anna <annkrv80@yandex.ru>
2023-06-12 12:35:47 +0700

    the task #2 from HW_2 was complited

--- HW_2/task_02.py
+++ HW_2/task_02.py
@@ -9,24 +9,13 @@ num = int(input('Введите число: '))
 def conver(num, system):
     result = list()
     while num > 0:
-        if num % system == 10:
-            result.append('a')
-        elif num % system == 11:
-            result.append('b')
-        elif num % system == 12:
-            result.append('c')
-        elif num % system == 13:
-            result.append('d')
-        elif num % system == 14:
-            result.append('e')
-        elif num % system == 15:
-            result.append('f')
-        else:
-            result.append(str(num % system))
+        hex_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c' , 'd', 'e', 'f']
+        result.append(hex_list[num % system])
         num = num // system
-    result.reverse()
+    result.reverse()   
     return ''.join(result)
+    
 
 
-print(f"Число {num} в двоичной системе {hex(num)}")
+print(f"Число {num} в шестнадцатиричной системе {hex(num)}")
 print(conver(num, 16))
