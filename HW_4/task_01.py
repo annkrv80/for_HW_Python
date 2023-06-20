195cf67cedac81bfec3d8dff2d6e1798808ec647
Anna <annkrv80@yandex.ru>
2023-06-20 22:47:42 +0700

    the task#1 from HW_4  was complited

---
+++ HW_4/task_01.py
@@ -0,0 +1,26 @@
+#Напишите функцию для транспонирования матрицы
+
+def transport_matrix(matrix: list):
+    n_rows = len(matrix)
+    m_colum = len(matrix[0])
+    result =[[ 0 for _ in range(n_rows)] for _ in range(m_colum)]
+
+    for i in range(n_rows):
+        for j in range(m_colum):
+            result[j][i] = matrix[i][j]
+    return result
+
+
+def print_matrix(matrix: list):
+    for i in range(len(matrix)):
+        print(matrix[i])
+
+
+mat = [[10, 20, 15, 35], [4, 5, 25, 33], [7, 8, 88, 77]]
+
+print('Исходная матрица')
+print_matrix(mat)
+
+mat_t = transport_matrix(mat)
+print('Транспорированная матрица')
+print_matrix(mat_t)
