139d6609e34981cca6c98ba19cf1db7ef2e7cc27
Anna <annkrv80@yandex.ru>
2023-06-25 02:25:30 +0700

    the task#7 from HW_4  was complited

--- HW_4/task_07.py
+++ HW_4/task_07.py
@@ -7,7 +7,7 @@ company_dict = {'Aeroflot': [1_000, -15_000, 25_000], 'S7': [5_000, -10_000, 6_0
                 'Pobeda': [3_000, 4_000, -2_000]}
 
 
-if any(map(lambda x: sum(x[1]) < 0, company_dict.items())):
+if any(map(lambda x: sum(x) < 0, company_dict.values())):
     print('Хотя бы одина компания убыточная')
 else:
     print('Все компании приносят прибыль')
