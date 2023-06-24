52c82925caa1468c1dd50614094ebd5288bad15a
Anna <annkrv80@yandex.ru>
2023-06-25 02:25:56 +0700

    the task#8 from HW_4  was complited

--- HW_4/task_08.py
+++ HW_4/task_08.py
@@ -3,15 +3,27 @@
 #оканчивающихся на s (кроме переменной из одной буквы s) на None.
 # Значения не удаляются, а помещаются в одноимённые переменные без s на конце.
 
-
-def no_s(**kwargs):
-    new_dict = {}
-    for key, value in kwargs.items():
-        if key[-1] == 's' and len(key) != 1:
-            new_dict[key[:-1]] = value
-            value = None
-        print(f' {key} = {value}')
-    print(f'Переменные c s на конце {new_dict}')
+names = 'Petr'
+a = 6
+bs = 33
+d = 44
+str = 'Hello'
+s = 4
+new_dict = {}
 
 
-no_s(names='Petr', a=6, bs=33, d=44, str='Hello', s=4)
+def no_s():
+   for key, value in globals().items():
+       if key[-1] == 's' and len(key) != 1:
+           globals()[key] = None
+           new_dict[key[:-1]] = value
+
+
+print(names)
+no_s()
+print(names)
+print(f'Переменные c s на конце {new_dict}')
+
+
+
+
